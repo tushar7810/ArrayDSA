437bc63b76049701312e71c538d79029149cb637
tushar7810 <141735609+tushar7810@users.noreply.github.com>
2024-02-06 10:38:22 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
-int BinarySearch(int arr[],int n,int num){
-    /*It returns the index of the founded number in this array*/
-    for(int i =0 ;i<n;i++){
-        if(arr[i] == num){
-            return i;
-        }
+int BinarySearch(int a[],int l,int r,int target){ // Time Complexity is O(log n) 
+    // here l is the low index and r is the last index of the array
+    while(l<=r){ 
+        int m = (l+(r-l))/2;
+        if(a[m] == target) return m;
+        else if(a[m] < target) r=m-1;
+        else l = m+1;
     }
     return -1;
 }
